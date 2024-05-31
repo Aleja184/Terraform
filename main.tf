a6776933c22c3b345af2f854055170fbeddc3ee0
Alejandra Toro <torogrisalesa@gmail.com>
2024-05-31 18:56:47 -0500

    Tags

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 ####### provider #######
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
+
 
 #######  resource ####### 
 resource "aws_instance" "nginx-server" {
-  ami           = "ami-0440d3b780d96b29d"
+  ami           = "ami-0b8b44ec9a8f90422"
   instance_type = "t3.micro"
-
   user_data = <<-EOF
               #!/bin/bash
               sudo yum install -y nginx
@@ -21,6 +21,13 @@ resource "aws_instance" "nginx-server" {
 	aws_security_group.nginx-server-sg.id
   ]
 
+    tags = {
+    Name        = "nginx-server"
+    Environment = "test"
+    Owner       = "torogrisalesa@gmail.com"
+    Team        = "DevOps"
+    Project     = "webinar"
+  }
 }
 
 ####### ssh ####### 
@@ -29,6 +36,15 @@ resource "aws_instance" "nginx-server" {
 resource "aws_key_pair" "nginx-server-ssh" {
   key_name   = "nginx-server-ssh"
   public_key = file("nginx-server.key.pub")
+
+  
+  tags = {
+    Name        = "nginx-server-ssh"
+    Environment = "test"
+    Owner       = "torogrisalesa@gmail.com"
+    Team        = "DevOps"
+    Project     = "webinar"
+  }
 }
 
 ####### SG ####### 
@@ -42,18 +58,25 @@ resource "aws_security_group" "nginx-server-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ##Regla de entrada
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
+  ##Regla de salida
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name        = "nginx-server-sg"
+    Environment = "test"
+    Owner       = "torogrisalesa@gmail.com"
+    Team        = "DevOps"
+    Project     = "webinar"
   }
 }
